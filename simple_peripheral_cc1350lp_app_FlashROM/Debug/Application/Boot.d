14110f0e0f09fa391b3036b67ed2d67f635f2065
altdor <altdor8@gmail.com>
2018-03-10 01:03:02 +0200

    Add files via upload

---
+++ simple_peripheral_cc1350lp_app_FlashROM/Debug/Application/Boot.d
@@ -0,0 +1,113 @@
+# FIXED
+
+Application/Boot.obj: C:/ti/simplelink_cc13x0_sdk_1_50_00_08/kernel/tirtos/packages/ti/sysbios/family/arm/msp432e4/init/Boot.c
+Application/Boot.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h
+Application/Boot.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdbool.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/std.h
+Application/Boot.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdarg.h
+Application/Boot.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stddef.h
+Application/Boot.obj: C:/ti/simplelink_cc13x0_sdk_1_50_00_08/kernel/tirtos/packages/ti/targets/arm/elf/std.h
+Application/Boot.obj: C:/ti/simplelink_cc13x0_sdk_1_50_00_08/kernel/tirtos/packages/ti/targets/arm/elf/M3.h
+Application/Boot.obj: C:/ti/simplelink_cc13x0_sdk_1_50_00_08/kernel/tirtos/packages/ti/targets/std.h
+Application/Boot.obj: C:/ti/simplelink_cc13x0_sdk_1_50_00_08/kernel/tirtos/packages/ti/sysbios/family/arm/msp432e4/init/hw_sysctl.h
+Application/Boot.obj: C:/ti/simplelink_cc13x0_sdk_1_50_00_08/kernel/tirtos/packages/ti/sysbios/family/arm/msp432e4/init/hw_types.h
+Application/Boot.obj: C:/ti/simplelink_cc13x0_sdk_1_50_00_08/kernel/tirtos/packages/ti/sysbios/family/arm/msp432e4/init/sysctl.h
+Application/Boot.obj: C:/ti/simplelink_cc13x0_sdk_1_50_00_08/kernel/tirtos/packages/ti/sysbios/family/arm/msp432e4/init/package/internal/Boot.xdc.h
+Application/Boot.obj: C:/ti/simplelink_cc13x0_sdk_1_50_00_08/kernel/tirtos/packages/ti/sysbios/family/arm/msp432e4/init/Boot.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/xdc.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types__prologue.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/package/package.defs.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types__epilogue.h
+Application/Boot.obj: C:/ti/simplelink_cc13x0_sdk_1_50_00_08/kernel/tirtos/packages/ti/sysbios/family/arm/msp432e4/init/package/package.defs.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IModule.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IHeap.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IInstance.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IModule.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Error.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Error__prologue.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Main.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IModule.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IGateProvider.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IInstance.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IModule.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/package/Main_Module_GateProxy.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IInstance.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IGateProvider.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IModule.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Error__epilogue.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Memory.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IModule.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IHeap.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Error.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/package/Memory_HeapProxy.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IInstance.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IHeap.h
+Application/Boot.obj: C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Error.h
+
+C:/ti/simplelink_cc13x0_sdk_1_50_00_08/kernel/tirtos/packages/ti/sysbios/family/arm/msp432e4/init/Boot.c: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdbool.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/std.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdarg.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stddef.h: 
+C:/ti/simplelink_cc13x0_sdk_1_50_00_08/kernel/tirtos/packages/ti/targets/arm/elf/std.h: 
+C:/ti/simplelink_cc13x0_sdk_1_50_00_08/kernel/tirtos/packages/ti/targets/arm/elf/M3.h: 
+C:/ti/simplelink_cc13x0_sdk_1_50_00_08/kernel/tirtos/packages/ti/targets/std.h: 
+C:/ti/simplelink_cc13x0_sdk_1_50_00_08/kernel/tirtos/packages/ti/sysbios/family/arm/msp432e4/init/hw_sysctl.h: 
+C:/ti/simplelink_cc13x0_sdk_1_50_00_08/kernel/tirtos/packages/ti/sysbios/family/arm/msp432e4/init/hw_types.h: 
+C:/ti/simplelink_cc13x0_sdk_1_50_00_08/kernel/tirtos/packages/ti/sysbios/family/arm/msp432e4/init/sysctl.h: 
+C:/ti/simplelink_cc13x0_sdk_1_50_00_08/kernel/tirtos/packages/ti/sysbios/family/arm/msp432e4/init/package/internal/Boot.xdc.h: 
+C:/ti/simplelink_cc13x0_sdk_1_50_00_08/kernel/tirtos/packages/ti/sysbios/family/arm/msp432e4/init/Boot.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/xdc.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types__prologue.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/package/package.defs.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types__epilogue.h: 
+C:/ti/simplelink_cc13x0_sdk_1_50_00_08/kernel/tirtos/packages/ti/sysbios/family/arm/msp432e4/init/package/package.defs.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IHeap.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IInstance.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Error.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Error__prologue.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Main.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IGateProvider.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IInstance.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/package/Main_Module_GateProxy.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IInstance.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IGateProvider.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Error__epilogue.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Memory.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IHeap.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Error.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/package/Memory_HeapProxy.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IInstance.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/IHeap.h: 
+C:/ti/xdctools_3_50_03_33_core/packages/xdc/runtime/Error.h: 
